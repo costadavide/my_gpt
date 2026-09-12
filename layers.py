293beb97da0d2fc68b0f9c39148dff9b62cd6494
shortstax23 <davide.costa2@studbocconi.it>
2026-09-12 16:07:01 +0200

    SoftMax class: V1 of the backward pass (using for loops)

---
+++ layers.py
@@ -0,0 +1,81 @@
+import numpy as np 
+
+
+
+class Linear: 
+    def __init__(self):
+        pass 
+
+class LayerNorm: 
+    def __init__(self):
+        pass 
+
+class FeedForward: 
+    def __init__(self):
+        pass 
+
+class SoftMax:
+    def __init__(self, axis=-1):
+        self.axis = axis
+        self.output = None # need to store the output for the backward pass 
+
+    def forward(self, x): 
+        # compute softmax to get a probability distribution for the next token 
+        # subtract the max value to avoid overflow in the exponential function
+        shifted_x = x - np.max(x, axis=self.axis, keepdims=True)
+        exp_x = np.exp(shifted_x)
+        sigma = exp_x / np.sum(exp_x, axis=self.axis, keepdims=True)
+        self.output = sigma
+        return sigma
+
+    def backward(self, grad_output): 
+        # V_1: explicitly compute the Jacobian matrix 
+        J = np.zeros((self.output.shape[0], self.output.shape[0]))
+        for i in range(self.output.shape[0]): 
+            for j in range(self.output.shape[0]): 
+                if i == j: 
+                    J[i, j] = self.output[i] * (1 - self.output[i])
+                else: 
+                    J[i, j] = -self.output[i] * self.output[j]
+
+        grad_input = np.dot(J.T, grad_output)
+        return grad_input 
+
+class EmbeddingLayer: 
+    def __init__(self):
+        pass 
+
+class MultiHeadAttention:
+    def __init__(self):
+        pass 
+
+
+x = np.array([0.5, -1.0, 2.0])
+grad_output = np.array([0.3, -0.7, 1.2])
+
+softmax = SoftMax()
+
+softmax.forward(x)
+analytical = softmax.backward(grad_output)
+
+def f(x):
+    y = softmax.forward(x)
+    return np.sum(y * grad_output)
+
+eps = 1e-5
+numerical = np.zeros_like(x)
+
+for i in range(len(x)):
+    x_plus = x.copy()
+    x_minus = x.copy()
+
+    x_plus[i] += eps
+    x_minus[i] -= eps
+
+    numerical[i] = (
+        f(x_plus) - f(x_minus)
+    ) / (2 * eps)
+
+print("Analytical:", analytical)
+print("Numerical: ", numerical)
+print("Difference:", analytical - numerical)
