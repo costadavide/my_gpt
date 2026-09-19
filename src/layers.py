bbc825906d748925dc47621944aa7c63c8f63259
costadavide <davide.costa2@studbocconi.it>
2026-09-19 21:40:13 +0200

    Added GELU activation function (same used in GPT2)

--- src/layers.py
+++ src/layers.py
@@ -64,6 +64,21 @@ class LayerNorm:
 
         return grad_input
 
+class GELU:
+    def __init__(self): 
+        self.x = None 
+    def forward(self, x):
+        self.x = x 
+        gelu = 0.5 * x * (1 +np.tanh(np.sqrt(2/np.pi) * (x+0.044715*x**3)))
+        return gelu 
+    
+    def backward(self, grad_output): 
+        x = self.x 
+        u = np.sqrt(2/np.pi)*(x+0.044715*x**3)
+        tanh_u = np.tanh(u)
+        grad_input = grad_output * (0.5 * (1 + tanh_u) + x/2 * (1 - tanh_u**2) * np.sqrt(2/np.pi) * (1 + 3*0.044715*x**2))
+        return grad_input
+
 class FeedForward: 
     def __init__(self):
         pass 
